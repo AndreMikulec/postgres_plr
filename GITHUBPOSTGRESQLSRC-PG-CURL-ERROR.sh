7e4757557470787ceeb659f64a92b87f360ef956
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-03-23 00:32:26 -0500

    Try again

--- GITHUBPOSTGRESQLSRC-PG-CURL-ERROR.sh
+++ GITHUBPOSTGRESQLSRC-PG-CURL-ERROR.sh
@@ -1,9 +1,12 @@
 
+set -v -x
+
+cd "$(dirname "$0")"
 
 curl -o THROWAWAYFILE --silent --head --fail -L ${PG_CURL_REMOTE_URL}
-if [ $? -eq  0 ]
+if [ $? -eq 0 ]
 then
   echo false > $(cygpath ${APPVEYOR_BUILD_FOLDER})/PG_CURL_ERROR.txt
-elif
+else
   echo true  > $(cygpath ${APPVEYOR_BUILD_FOLDER})/PG_CURL_ERROR.txt
 fi
